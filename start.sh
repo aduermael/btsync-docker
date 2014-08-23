bdc5cf17178e290cd4d380c02f7e068f199878f6
Adrien Duermael <ad@pixowl.com>
2014-08-23 23:53:33 +0200

    remove .SyncID before adding directory

--- start.sh
+++ start.sh
@@ -4,4 +4,6 @@ sed "s,SECRET_FIELD,$2," < config.tmp1 > config.tmp2
 rm config
 rm config.tmp1
 mv config.tmp2 config
+# In case the directory was synced before with another SyncID
+rm $1/.SyncID
 btsync --config config --nodaemon
