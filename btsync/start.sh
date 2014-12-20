3b94d148d7e3128dd5f2871f8453a328c32a08c8
Adrien Duermael <ad@pixowl.com>
2014-12-19 21:22:17 -0800

    Run container with folder and key

--- btsync/start.sh
+++ btsync/start.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# sed "s,DIR_FIELD,$1," < config > config.tmp1
-# sed "s,SECRET_FIELD,$2," < config.tmp1 > config.tmp2
-# rm config
-# rm config.tmp1
-# mv config.tmp2 config
-
-# In case the directory was synced before with another SyncID
-# rm $1/.SyncID
-
-# btsync --config config --nodaemon
-
 nodejs btsync.js init
+
+if [ "$#" -eq 2 ]
+then
+	nodejs btsync.js add-folder $1 $2
+fi
+
 nodejs btsync.js restart
 ./console.sh
 
